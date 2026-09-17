0e06dd8c18e67e35c6133c69df1b4c2b41779839
guktio <guktio@proton.me>
2026-09-17 16:09:42 +0300

    aded auth exeception handler

--- src/main/java/com/fitness/application/gym/plan/dto/CreatePlanDTO.java
+++ src/main/java/com/fitness/application/gym/plan/dto/CreatePlanDTO.java
@@ -1,10 +1,14 @@
 package com.fitness.application.gym.plan.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data 
 @Builder
+@NoArgsConstructor 
+@AllArgsConstructor
 public class CreatePlanDTO {
     private String title;
 
--- src/main/java/com/fitness/application/gym/set/dto/CreateSetDTO.java
+++ src/main/java/com/fitness/application/gym/set/dto/CreateSetDTO.java
@@ -1,9 +1,13 @@
 package com.fitness.application.gym.set.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder 
 public class CreateSetDTO {
     private Integer setNumber;
